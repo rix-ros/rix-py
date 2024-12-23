bcdf126d193fc05602f3b1fc00eb189027033456
Broderick <broderio@umich.edu>
2024-12-23 16:26:18 -0500

    add install

--- install.sh
+++ install.sh
@@ -1,1 +1,7 @@
 cp -r rixcore ~/.rix/python/
+python3 -m venv ~/.rix/venv
+source ~/.rix/venv/bin/activate
+pip install pyinstaller
+pip install ~/.rix/python/rixmsg
+pip install ~/.rix/python/rixcore
+deactivate
