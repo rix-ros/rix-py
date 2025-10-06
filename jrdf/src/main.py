fcf700d00f3a95629ef9a072cce491d1fdbbdacb
Broderick <broderio@umich.edu>
2025-10-06 16:06:10 -0400

    remove json macro parser from jrdf and add as separate dependency

--- jrdf/src/main.py
+++ jrdf/src/main.py
@@ -5,297 +5,123 @@ import os
 rix_path = os.path.expanduser("~/.rix/python/rix")
 sys.path.append(rix_path)
 
-from open3d_util import Open3DRobotModel
-
+from typing import Any
 import json
 import jsonschema
+from jsonmacros import JsonMacroProcessor, JsonMergeStrategy
 import argparse
-import open3d as o3d
-from urdf_parser_py.urdf import URDF
+from urdf_converter import URDFConverter
 
 
 HOME = os.path.expanduser("~")
 USAGE = """jrdf [-h] function [arg]
 
 Functions:
-  create <name> <json | urdf> [asset directory]    - Create a JRDF model
+  create <model directory>                         - Create a JRDF model
+  install <model directory>                        - Install a JRDF model
+  uninstall <name>                                 - Uninstall a JRDF model
+  convert <urdf file> [asset directory]            - Convert a URDF to JRDF
   list                                             - List all JRDF models
   validate <input.json>                            - Validate a JRDF JSON file
   visualize <name>                                 - Visualize a JRDF model
 """
 
 
-def find_file(filename: str, path: str) -> str | None:
-    for entry in os.listdir(path):
-        full_path = os.path.join(path, entry)
-        if os.path.isdir(full_path):
-            file = find_file(filename, full_path)
-            if file is not None:
-                return file
-        elif full_path.endswith(filename):
-            return full_path
-    return None
+def create(model_dir: str) -> None:
+    # Create the model directory if it doesn't exist
+    if not os.path.isdir(model_dir):
+        os.makedirs(model_dir, exist_ok=True)
+        print(f"Created model directory '{model_dir}'")
+    else:
+        print(f"Model directory '{model_dir}' already exists.")
 
+    # Create the assets directory if it doesn't exist
+    assets_dir = os.path.join(model_dir, "assets")
+    if not os.path.isdir(assets_dir):
+        os.makedirs(assets_dir, exist_ok=True)
+        print(f"Created assets directory '{assets_dir}'")
+    else:
+        print(f"Assets directory '{assets_dir}' already exists.")
 
-def find_model_file(name: str, filename: str) -> str | None:
-    path = find_file(
-        os.path.basename(filename), HOME + "/.rix/jrdf/models/" + name + "/assets"
-    )
-    if path is None:
-        return None
-    return path.split(HOME + "/.rix/jrdf/models/" + name + "/assets/")[1]
-
-
-def convert_urdf_to_jrdf(name: str, urdf_file: str) -> dict:
-    # Code here
-    jrdf: dict = {
-        "name": "",
-        "links": [],
-        "joints": [],
-    }
-
-    # Use URDF library to store data in the jrdf dict
-    robot = URDF.from_xml_file(urdf_file)
-
-    jrdf["name"] = robot.name
-
-    # Parse global materials
-    if len(robot.materials) > 0:
-        jrdf["materials"] = []
-        for material in robot.materials:
-            tmp_material = {"name": material.name}
-            if material.color is not None:
-                tmp_material["color"] = material.color.rgba
-            if material.texture is not None:
-                tmp_material["filename"] = material.texture.filename
-            jrdf["materials"].append(tmp_material)
-
-    # Use JSON library to write jrdf dict to file as JSON
-    for link in robot.links:
-        link_dict = {
-            "name": link.name,
+    # Create a default JRDF file if it doesn't exist
+    jrdf_file = os.path.join(model_dir, "model.json")
+    if not os.path.isfile(jrdf_file):
+        default_jrdf = {
+            "name": os.path.basename(model_dir),
+            "links": [],
+            "joints": [],
         }
+        with open(jrdf_file, "w") as f:
+            json.dump(default_jrdf, f, indent=4)
+        print(f"Created default JRDF file '{jrdf_file}'")
+    else:
+        print(f"JRDF file '{jrdf_file}' already exists.")
 
-        if link.visuals:
-            # For each visual, add a visual object to the list within the link
-            visuals_list = []
-            for visual in link.visuals:
-                geom_type = type(visual.geometry).__name__.lower()
-
-                if geom_type == "mesh":
-                    filename = find_model_file(name, visual.geometry.filename)
-                    if filename is None:
-                        print(
-                            'Warning! Unable to find model "'
-                            + visual.geometry.filename
-                            + '".'
-                        )
-                        filename = ""
-
-                    geometry = {
-                        "type": geom_type,
-                        "filename": filename,
-                        "scale": (
-                            visual.geometry.scale
-                            if visual.geometry.scale
-                            else [1, 1, 1]
-                        ),
-                    }
-                elif geom_type == "box":
-                    geometry = {"type": geom_type, "size": visual.geometry.size}
-                elif geom_type == "cylinder":
-                    geometry = {
-                        "type": geom_type,
-                        "radius": visual.geometry.radius,
-                        "length": visual.geometry.length,
-                    }
-                elif geom_type == "sphere":
-                    geometry = {"type": geom_type, "radius": visual.geometry.radius}
-
-                origin = {
-                    "xyz": visual.origin.xyz if visual.origin else [0, 0, 0],
-                    "rpy": visual.origin.rpy if visual.origin else [0, 0, 0],
-                }
-
-                material = {}
-                if visual.material:
-                    material["name"] = visual.material.name
-                    if visual.material.texture:
-                        material["filename"] = visual.material.texture.filename
-                    if visual.material.color:
-                        material["color"] = visual.material.color.rgba
-
-                visual_obj = {
-                    "geometry": geometry,
-                    "origin": origin,
-                    "material": material,
-                }
-                visuals_list.append(visual_obj)
-
-            link_dict["visuals"] = visuals_list
-
-        # Add support for multiple collision properties
-        if link.collisions:
-            collisions_list = []
-            for collision in link.collisions:
-                geom_type = type(collision.geometry).__name__.lower()
-
-                if geom_type == "mesh":
-                    filename = find_model_file(name, visual.geometry.filename)
-                    if filename is None:
-                        print(
-                            'Warning! Unable to find model "'
-                            + visual.geometry.filename
-                            + '".'
-                        )
-                        filename = ""
-
-                    geometry = {
-                        "type": geom_type,
-                        "filename": filename,
-                        "scale": (
-                            visual.geometry.scale
-                            if visual.geometry.scale
-                            else [1, 1, 1]
-                        ),
-                    }
-                elif geom_type == "box":
-                    geometry = {
-                        "type": geom_type,
-                        "size": collision.geometry.size,
-                    }
-                elif geom_type == "cylinder":
-                    geometry = {
-                        "type": geom_type,
-                        "radius": collision.geometry.radius,
-                        "length": collision.geometry.length,
-                    }
-                elif geom_type == "sphere":
-                    geometry = {
-                        "type": geom_type,
-                        "radius": collision.geometry.radius,
-                    }
-
-                origin = {
-                    "xyz": collision.origin.xyz,
-                    "rpy": collision.origin.rpy,
-                }
-
-                collision_obj = {
-                    "geometry": geometry,
-                    "origin": origin,
-                    "material": material,
-                }
-                collisions_list.append(collision_obj)
-
-            link_dict["collisions"] = collisions_list
-
-        # Add support for intertia property
-        if link.inertial:
-            inertial = link.inertial
-            link_dict["inertial"] = {
-                "origin": {
-                    "xyz": (
-                        inertial.origin.xyz
-                        if inertial.origin is not None
-                        else [0, 0, 0]
-                    ),
-                    "rpy": (
-                        inertial.origin.rpy
-                        if inertial.origin is not None
-                        else [0, 0, 0]
-                    ),
-                },
-                "mass": inertial.mass,
-                "inertia": {
-                    "ixx": inertial.inertia.ixx,
-                    "ixy": inertial.inertia.ixy,
-                    "ixz": inertial.inertia.ixz,
-                    "iyy": inertial.inertia.iyy,
-                    "iyz": inertial.inertia.iyz,
-                    "izz": inertial.inertia.izz,
-                },
-            }
-
-        jrdf["links"].append(link_dict)
-
-    # Parse joints
-    for joint in robot.joints:
-        joint_dict = {
-            "name": joint.name,
-            "type": joint.type,
-            "parent": joint.parent,
-            "child": joint.child,
-            "origin": {
-                "xyz": joint.origin.xyz if joint.origin else [0, 0, 0],
-                "rpy": joint.origin.rpy if joint.origin else [0, 0, 0],
-            },
-            "axis": joint.axis if joint.axis else [1, 0, 0],
-        }
-        if joint.limit is not None:
-            joint_dict["limits"] = {
-                "effort": joint.limit.effort,
-                "velocity": joint.limit.velocity,
-            }
-            if joint.limit.lower is not None:
-                joint_dict["limits"]["lower"] = joint.limit.lower
-            if joint.limit.upper is not None:
-                joint_dict["limits"]["upper"] = joint.limit.upper
-
-        if joint.dynamics is not None:
-            joint_dict["dynamics"] = {}
-            if joint.dynamics.damping is not None:
-                joint_dict["dynamics"]["damping"] = joint.dynamics.damping
-            if joint.dynamics.friction is not None:
-                joint_dict["dynamics"]["friction"] = joint.dynamics.friction
-
-        if joint.mimic is not None:
-            joint_dict["mimic"] = {}
-            if joint.mimic.multiplier is not None:
-                joint_dict["mimic"]["multiplier"] = joint.mimic.multiplier
-            if joint.mimic.offset is not None:
-                joint_dict["mimic"]["offset"] = joint.mimic.offset
-            if joint.mimic.joint is not None:
-                joint_dict["mimic"]["name"] = joint.mimic.joint
-
-        jrdf["joints"].append(joint_dict)
-
-    return jrdf
-
-
-def create(name: str, input_file: str, asset_dir: str | None) -> None:
-    if not os.path.isfile(input_file):
-        print(f"Error! Input file '{input_file}' does not exist.")
+def install(model_dir: str) -> None:
+    # Install the model by creating a symlink in the ~/.rix/jrdf/models directory
+    if not os.path.isdir(model_dir):
+        print(f"Error! Model directory '{model_dir}' does not exist.")
+        return
+    
+    jrdf_file = os.path.join(model_dir, "model.json")
+    # Find all .json files in model_dir
+    json_files = [f for f in os.listdir(model_dir) if f.endswith(".json") and f != "model.json"]
+    if len(json_files) == 0:
+        print(f"Error! No JRDF file found in '{model_dir}'.")
+    
+    try:
+        processor = JsonMacroProcessor(merge_strategy=JsonMergeStrategy.MERGE_WITH_ERROR)
+        for json_file in json_files:
+            processor.process_file(os.path.join(model_dir, json_file))
+    except Exception as e:
+        print(f"Error processing JSON macros in '{model_dir}': {e}")
         return
 
-    # If provided, copy asset files to ~/.rix/jrdf/models/<name>/assets/
-    if asset_dir:
-        if not os.path.isdir(asset_dir):
-            print(f"Error! Asset directory '{asset_dir}' does not exist.")
-            return
-        os.system(f"mkdir -p {HOME}/.rix/jrdf/models/{name}/assets")
-        os.system(f"cp -r {asset_dir} {HOME}/.rix/jrdf/models/{name}/assets")
-
-    # Determine if input is JSON or URDF
-    if not (
-        input_file.lower().endswith(".json") or input_file.lower().endswith(".urdf")
-    ):
-        print("Error! Input file must be a .json or .urdf file.")
-        return
-
-    jrdf: dict = {}
-    if input_file.lower().endswith(".json"):
-        with open(input_file, "r") as f:
+    properties: dict[str, Any] = {}
+    properties["$comment"] = "This file was generated by jrdf from jsonmacros. Do not edit directly."
+    properties.update(processor.properties)
+    with open(jrdf_file, "w") as f:
+        json.dump(properties, f, indent=4)
+    print(f"Created JRDF file '{jrdf_file}' from macros.")
+    
+    model_name: str
+    dest_dir: str
+    try:
+        with open(jrdf_file, "r") as f:
             jrdf = json.load(f)
-    if input_file.lower().endswith(".urdf"):
-        jrdf = convert_urdf_to_jrdf(name, input_file)
+        schema = {}
+        schema_file = os.path.join(HOME, ".rix", "jrdf", "jrdf_schema.json")
+        with open(schema_file, "r") as f:
+            schema = json.load(f)
+        jsonschema.validate(instance=jrdf, schema=schema)
+        model_name = jrdf["name"]
+        models_dir = os.path.join(HOME, ".rix", "jrdf", "models")
+        if not os.path.isdir(models_dir):
+            os.makedirs(models_dir, exist_ok=True)
 
-    # Copy json to ~/.rix/jrdf/models/<name>/model.json
-    output_file = os.path.join(HOME, ".rix", "jrdf", "models", name, "model.json")
-    os.makedirs(os.path.dirname(output_file), exist_ok=True)
-    with open(output_file, "w") as f:
-        json.dump(jrdf, f, indent=4)
+        dest_dir = os.path.join(models_dir, model_name)
+        if os.path.islink(dest_dir) or os.path.isdir(dest_dir):
+            print(f"Model '{model_name}' is already installed. Uninstall it first.")
+            return
+    except Exception as e:
+        print(f"Error! JRDF file '{jrdf_file}' is not valid: {e}")
+        return
 
+    os.symlink(os.path.abspath(model_dir), dest_dir)
+    print(f"Installed model '{model_name}'.")
+
+def uninstall(name: str) -> None:
+    models_dir = os.path.join(HOME, ".rix", "jrdf", "models")
+    dest_dir = os.path.join(models_dir, name)
+    if not os.path.islink(dest_dir) and not os.path.isdir(dest_dir):
+        print(f"Model '{name}' is not installed.")
+        return
+
+    if os.path.islink(dest_dir):
+        os.unlink(dest_dir)
+    else:
+        os.system(f"rm -r {dest_dir}")
+    print(f"Uninstalled model '{name}'.")
 
 def list() -> None:
     models_dir = os.path.join(HOME, ".rix", "jrdf", "models")
@@ -317,6 +143,44 @@ def list() -> None:
         print(name)
 
 
+def convert(urdf_path: str, asset_dir: str | None) -> None:
+    if not os.path.isfile(urdf_path):
+        print(f"Error! URDF file '{urdf_path}' does not exist.")
+        return
+
+    if asset_dir and not os.path.isdir(asset_dir):
+        print(f"Error! Asset directory '{asset_dir}' does not exist.")
+        return
+
+    converter = URDFConverter()
+    try:
+        jrdf, assets = converter.convert(urdf_path, asset_dir)
+
+        # Create output directory ./<name> if it doesn't exist
+        output_dir = os.path.join(".", jrdf["name"].lower())
+        os.makedirs(output_dir, exist_ok=True)
+        output_file = os.path.join(output_dir, "model.json")
+        with open(output_file, "w") as f:
+            json.dump(jrdf, f, indent=4)
+
+        # Validate the generated JRDF
+        schema = {}
+        schema_file = os.path.join(HOME, ".rix", "jrdf", "jrdf_schema.json")
+        with open(schema_file, "r") as f:
+            schema = json.load(f)
+        jsonschema.validate(instance=jrdf, schema=schema)
+        print(f"Converted URDF to JRDF and saved to '{output_file}'")
+
+        # Copy assets if any
+        if assets and asset_dir:
+            dest_dir = os.path.join(output_dir, "assets")
+            os.makedirs(dest_dir, exist_ok=True)
+            os.system(f"cp -r {asset_dir}/* {dest_dir}/")
+
+    except Exception as e:
+        print(f"Error converting URDF to JRDF: {e}")
+
+
 def validate(jrdf_file: str) -> None:
     try:
         with open(jrdf_file, "r") as f:
@@ -334,6 +198,8 @@ def validate(jrdf_file: str) -> None:
 
 
 def visualize(name: str) -> None:
+    from open3d_util import Open3DRobotModel
+    import open3d as o3d
     try:
         print(f"Visualizing JRDF model '{name}'...")
         vis = o3d.visualization.Visualizer()
@@ -351,17 +217,40 @@ def main(args: argparse.Namespace) -> None:
     function = args.function
 
     if function == "create":
-        if len(args.args) < 2:
-            print("Error! 'create' requires at least 2 arguments: <name> <json | urdf>")
+        if len(args.args) < 1:
+            print("Error! 'create' requires at least 1 argument: <model directory>")
+            return
+        model_dir = args.args[0]
+        create(model_dir)
+        return
+    
+    if function == "install":
+        if len(args.args) < 1:
+            print("Error! 'install' requires at least 1 argument: <model directory>")
+            return
+        model_dir = args.args[0]
+        install(model_dir)
+        return
+
+    if function == "uninstall":
+        if len(args.args) < 1:
+            print("Error! 'uninstall' requires at least 1 argument: <name>")
             return
         name = args.args[0]
-        input_file = args.args[1]
-        if not input_file:
-            print("Error! 'create' requires an input JSON or URDF file as an argument.")
+        uninstall(name)
+        return
+
+    if function == "convert":
+        if len(args.args) < 1:
+            print("Error! 'convert' requires at least 1 argument: <urdf file>")
+            return
+        urdf_file = args.args[0]
+        if not urdf_file:
+            print("Error! 'convert' requires an input URDF file as an argument.")
             return
 
-        asset_dir = args.args[2] if len(args.args) > 2 else None
-        create(name, input_file, asset_dir)
+        asset_dir = args.args[1] if len(args.args) > 1 else None
+        convert(urdf_file, asset_dir)
         return
 
     if function == "list":
